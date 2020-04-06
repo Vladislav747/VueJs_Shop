cc1a0b3515c2941d6341f9a1c54f0d4922194571
Vladislav_747 <zippo585@gmail.com>
2020-04-06 14:35:36 +0500

    Deploy fixes 2

--- src/config/clientConfigs.js
+++ src/config/clientConfigs.js
@@ -1,5 +1,5 @@
 module.exports = {
   //!!!It matches the port we launch node server
     baseURLDevelopment: 'http://localhost:8080/',
-    baseURLProduction: 'https://vue-taskmanager.herokuapp.com/',
+    baseURLProduction: 'https://napoleon-shop.herokuapp.com/',
   };
