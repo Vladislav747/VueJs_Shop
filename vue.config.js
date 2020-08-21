d070f7c41018c5dbbc37f286f1f58e0e1140112a
Vladislav_747 <zippo585@gmail.com>
2020-08-21 18:27:44 +0500

    Deploy problem

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,11 @@ module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
+  //Чтобы использовать для github pages
   publicPath: process.env.NODE_ENV === 'production' ?
     '../dist/' :
     '/',
     
-    outputDir:path.resolve(__dirname, 'vuejs_shop','../dist/')
+    outputDir:path.resolve(__dirname, '../dist/')
   
 }
